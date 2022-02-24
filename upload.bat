2278e6b603b0e78a78f33a5683d53b7b5c0c6498
muhuzhongxun <983724494@qq.com>
2022-02-24 20:17:40 +0800

    +

---
+++ upload.bat
@@ -0,0 +1,21 @@
+#@echo off
+
+echo.
+echo                                      GitHub自动上传脚本
+echo ===================================================================================
+echo.
+
+set /p change=%date:~0,10%
+echo.
+
+# git pull
+git add .
+git commit -m  ""+%change%
+git push
+
+echo.
+echo ===================================================================================
+echo                                      更新完毕
+echo.
+
+pause
