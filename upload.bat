78d8273ac063a1a0795ce917e4d8a961038c62cf
muhuzhongxun <983724494@qq.com>
2022-02-24 20:20:18 +0800

    2022/02/24

--- upload.bat
+++ upload.bat
@@ -5,12 +5,12 @@ echo                                      GitHub自动上传脚本
 echo ===================================================================================
 echo.
 
-set /p change=%date:~0,10%
+#set /p change=%date:~0,10%
 echo.
 
 # git pull
 git add .
-git commit -m  ""+%change%
+git commit -m  %date:~0,10%
 git push
 
 echo.
